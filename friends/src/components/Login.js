4c6681f70d5cec5ed0ed2f15bcebdf5e934f9fec
JM <manzur-1990@hotmail.com>
2020-07-21 14:06:57 -0500

    login is posting data

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 export const Login = () => {
+    const history = useHistory();
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:5000/api/login', credentials)
+            .then(res => {
+                console.log("from Login",res);
+                localStorage.setItem('token', res.data.payload);
+                history.push('/friends');
+
+            })
+            .catch(err => console.log(err.response));
+        setCredentials({
+            username: '',
+            password: ''
+        });
+    };
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
     return (
         <div>
-            
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    name='username'
+                    placeholder='enter username'
+                    value={credentials.username}
+                    onChange={onChange}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='enter password'
+                    value={credentials.password}
+                    onChange={onChange}
+                />
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
         </div>
-    )
-}
+    );
+};
